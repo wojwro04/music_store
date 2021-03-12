aaa7e4a626923bf0fc407975060a29efec7af863
Andrzej Szary <aszary@gmail.com>
2021-03-12 14:04:19 +0100

    new models

--- music/models.py
+++ music/models.py
@@ -1,13 +1,31 @@
 from django.db import models
 
 
+
+
+
+class Album(models.Model):
+    title = models.CharField(max_length=200)
+
+
+class Artist(models.Model):
+    pass
+
+
+class Track(models.Model):
+    pass
+
+
+
+
+
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
-    
+
     def __str__(self):
         return self.question_text
-    
+
     def was_published_recently(self):
         return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
@@ -16,6 +34,6 @@ class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
-    
+
     def __str__(self):
         return self.choice_text
