5453f32c63ced268d2c645164c524c338880a9d8
Andrzej Szary <aszary@gmail.com>
2021-03-12 14:21:15 +0100

    migrations made

--- music/admin.py
+++ music/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 
-from .models import Question
+from .models import Question, Album, Artist, Track
 
 admin.site.register(Question)
+admin.site.register(Album)
+admin.site.register(Artist)
+admin.site.register(Track)
