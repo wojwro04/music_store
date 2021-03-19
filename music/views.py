66aa5db9b76f8df530faafd9a16ef407c7dbafef
Andrzej Szary <aszary@gmail.com>
2021-03-19 14:01:26 +0100

    basic views added

--- music/views.py
+++ music/views.py
@@ -2,9 +2,15 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
+from .models import Album
+
 
 def index(request):
     return HttpResponse("Witaj na stronie z muzyką.")
 
 def albums(request):
-    return HttpResponse("Tutaj widoczne sa wszystkie albumy")
+    albums = Album.objects.all()
+    lista = ""
+    for album in albums:
+        lista += album.title+ "<br>"
+    return HttpResponse("Tutaj widoczne sa wszystkie albumy:<br> %s"% lista)
