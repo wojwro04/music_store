58498b7a705f1884920769e57ca693386bfabe64
Wojciech Wróblewski <wojwro04@gmail.com>
2021-03-19 14:10:23 +0100

    views added

--- music/urls.py
+++ music/urls.py
@@ -5,4 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('albums', views.albums, name='albums'),
+    path('tracks', views.tracks, name='tracks'),
+    path('artists', views.artists, name='artists'),
 ]
--- music/views.py
+++ music/views.py
@@ -3,6 +3,8 @@ from django.http import HttpResponse
 
 # Create your views here.
 from .models import Album
+from .models import Track
+from .models import Artist
 
 
 def index(request):
@@ -14,3 +16,17 @@ def albums(request):
     for album in albums:
         lista += album.title+ "<br>"
     return HttpResponse("Tutaj widoczne sa wszystkie albumy:<br> %s"% lista)
+
+def tracks(request):
+    tracks = Track.objects.all()
+    lista = ""
+    for track in tracks:
+        lista += track.name + "<br>"
+    return HttpResponse("Tutaj widoczne sa wszystkie utwory:<br> %s" % lista)
+
+def artists(request):
+    artists = Artist.objects.all()
+    lista = ""
+    for artist in artists:
+        lista += artist.name + "<br>"
+    return HttpResponse("Tutaj widoczne sa wszyscy artyści:<br> %s" % lista)
